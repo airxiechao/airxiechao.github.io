dc72d6e2c9e67e12fa3a552a8a64a1f4f23e28d3
airxiechao <airxiechao@qq.com>
2019-01-30 16:24:18 +0800

    oarword

---
+++ oarword/js/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: airxiechao@gmail.com
+ * Date: 13-8-9
+ * Time: 上午6:27
+ */
+
+var config = {
+    appbase: 'https://airxiechao.github.io/oarword'
+};
